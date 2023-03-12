11d7c94286d632f05b3c34eec965d5947e6e7d38
iRockE <jmrichards96@gmail.com>
2023-03-11 20:18:07 -0500

    configure async storage

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
 import {name as appName} from './app.json';
-import {store} from './src/app/store';
+import {store, persistor} from './src/app/store';
 
 const AppWithStore = () => (
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,37 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  REGISTER,
+  PAUSE,
+  PURGE,
+  PERSIST,
+} from 'redux-persist';
 import {groceryListsReducer} from '../features/groceryList/groceryListsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 const rootReducer = combineReducers({groceryLists: groceryListsReducer});
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof rootReducer>;
