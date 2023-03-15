01ead6acba456f68916cb01989f27fedcb22de8e
iRockE <jmrichards96@gmail.com>
2023-03-14 22:53:57 -0400

    render grocery lists in draggable flat list

--- src/screens/GroceryListManagerScreen.tsx
+++ src/screens/GroceryListManagerScreen.tsx
@@ -1,8 +1,13 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {Button, StyleSheet, View} from 'react-native';
+import DraggableFlatList, {
+  DragEndParams,
+  RenderItemParams,
+} from 'react-native-draggable-flatlist';
 import {useAppSelector} from '../app/hooks';
 import {getLists} from '../features/groceryList/groceryListsSlice';
+import {GroceryListData} from '../interfaces/interfaces';
 import {RootStackParamList} from '../navigation/PrimaryNavigation';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'GroceryListManager'>;
@@ -10,16 +15,28 @@ type Props = NativeStackScreenProps<RootStackParamList, 'GroceryListManager'>;
 const GroceryListManagerScreen = ({navigation}: Props): JSX.Element => {
   const groceryLists = useAppSelector(getLists);
 
+  const onDragEnd = ({data}: DragEndParams<GroceryListData>) => {};
+
   return (
     <View style={styles.container}>
-      {groceryLists.map(groceryList => (
-        <Button
-          onPress={() =>
-            navigation.navigate('GroceryList', {list: groceryList})
-          }
-          title={groceryList.title}
-        />
-      ))}
+      <DraggableFlatList
+        keyboardShouldPersistTaps={'handled'}
+        data={groceryLists}
+        renderItem={({
+          item,
+          drag,
+          isActive,
+        }: RenderItemParams<GroceryListData>) => (
+          <Button
+            onPress={() => {
+              navigation.push('GroceryList', {list: item});
+            }}
+            title={item.title}
+          />
+        )}
+        keyExtractor={(item: GroceryListData) => item.id}
+        onDragEnd={onDragEnd}
+      />
     </View>
   );
 };
